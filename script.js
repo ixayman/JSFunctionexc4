82c01ac6de9caa40e7cb9d86f6dc2ca3e802bacf
ayman khalel <evoix.a@gmail.com>
2016-08-29 15:31:16 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function show1()
+{
+	document.getElementById("isr").style.display="block";
+	
+}
+function show2()
+{
+	document.getElementById("usa").style.display="block";
+	
+}function show3()
+{
+	document.getElementById("ita").style.display="block";
+	
+}function show4()
+{
+	document.getElementById("net").style.display="block";
+	
+}function show5()
+{
+	document.getElementById("fra").style.display="block";
+	
+}
+function hide()
+{
+	document.getElementByClassName("pos").style.display="none";
+	//this func is not working.
+}
